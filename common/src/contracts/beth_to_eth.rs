b54082d054582ec14004432bcb540d9fd2a081ab
Ali Ghahremani <alighahremani1377@gmail.com>
2026-02-17 14:51:50 +0330

    fix: abi_encode + test

--- common/src/contracts/beth_to_eth.rs
+++ common/src/contracts/beth_to_eth.rs
@@ -43,12 +43,9 @@ impl BETHToETHContract {
     ) -> Result<Bytes, anyhow::Error> {
         let calldata = Self::create_swap_beth_with_eth_calldata(swap_amount, recipient);
 
-        Ok(<(Address, U256, Bytes) as SolValue>::abi_encode(&(
-            network.beth_to_eth_address()?,
-            swap_amount,
-            calldata.clone(),
-        ))
-        .into())
+        Ok((network.beth_to_eth_address()?, swap_amount, calldata)
+            .abi_encode_params()
+            .into())
     }
 
     pub fn create_swap_beth_with_eth_calldata(swap_amount: U256, recipient: Address) -> Bytes {
@@ -73,3 +70,24 @@ type BETHToETHContractType = BETHToETH::BETHToETHInstance<
         RootProvider,
     >,
 >;
+
+#[cfg(test)]
+mod tests {
+    use alloy::primitives::utils::parse_ether;
+
+    use super::*;
+
+    #[test]
+    fn swap_hook_test() {
+        assert_eq!(
+            BETHToETHContract::create_swap_hook(
+                Network::Mainnet,
+                parse_ether("0.0005").unwrap(),
+                address!("0x59Cd66cbb73D99e126Fa557E588f3d8dE8011b2C")
+            )
+            .unwrap()
+            .to_string(),
+            "0x000000000000000000000000ba5a285806c343aad955a40fe4b6e5e607b752b60000000000000000000000000000000000000000000000000001c6bf52634000000000000000000000000000000000000000000000000000000000000000006000000000000000000000000000000000000000000000000000000000000000441d20dd830000000000000000000000000000000000000000000000000001c6bf5263400000000000000000000000000059cd66cbb73d99e126fa557e588f3d8de8011b2c00000000000000000000000000000000000000000000000000000000".to_string()
+        )
+    }
+}
